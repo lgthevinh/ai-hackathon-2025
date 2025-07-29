76d407e236fe7c73bf7675d23d72678b2a1bbd3c
lgthvinh <everwellmax@gmail.com>
2025-07-30 00:54:57 +0700

    update docs

--- test/send_to_esp.py
+++ test/send_to_esp.py
@@ -8,8 +8,8 @@ SERIAL_PORT = 'COM11'      # Change to your serial port (e.g., '/dev/ttyUSB0' or
 BAUDRATE = 115200
 
 # --- Message values ---
-MSG_TYPE = 0x01          # Example: MCMD
-NUMBER = 0x04
+MSG_TYPE = 0x01          # Example: MCMD Motor Command
+NUMBER = 0x04 # Example: Motor number (0x04 for motor 4) 
 DIR = 0x01           # Example: Direction (0 for forward, 1 for backward)
 PULSE = 0x0FA0  # Example: 4000 in decimal
 
@@ -25,18 +25,6 @@ with serial.Serial(SERIAL_PORT, BAUDRATE, timeout=1) as ser:
     # Optionally read response
     if ser.in_waiting:
         response = ser.read(ser.in_waiting)
-        
-        # print("Received response:", [hex(b) for b in response])
-        # if len(response) > 0:
-        #     print("Response length:", len(response))
-        #     if len(response) >= 4:
-        #         msg_type, number, pulse = struct.unpack('<BBH', response[:4])
-        #         print(f"Parsed - Type: {msg_type}, Number: {number}, Pulse: {pulse}")
-        #     else:
-        #         print("Response too short to parse")
-        
-        # print("Response from ESP:", response.hex())
-        
         ascii_str = binascii.unhexlify(response.hex()).decode()
         print("Response from ESP:", ascii_str)
 
