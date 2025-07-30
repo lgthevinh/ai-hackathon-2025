2da134bbc823119477c10fcbdc1bdcb93f76aab1
lgthvinh <everwellmax@gmail.com>
2025-07-30 13:50:53 +0700

    add define motor and servo pins

--- main-aihack/robot_types.h
+++ main-aihack/robot_types.h
@@ -4,6 +4,20 @@
 #define BAUDRATE 115200
 #define MAX_DATA_SIZE 16
 
+#define M1_A 2
+#define M1_B 3
+#define M2_A 4
+#define M2_B 5
+#define M3_A 7
+#define M3_B 6
+#define M4_A 1
+#define M4_B 0
+#define SERVO_1 12
+#define SERVO_2 11
+#define SERVO_3 10
+#define SERVO_4 9
+#define SERVO_5 8
+
 enum message_type_t {
   MSG_TYPE_MCMD = 0x01,
   MSG_TYPE_SCMD = 0x02,
